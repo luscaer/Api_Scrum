9725454216052fc56d0cdc69b1847ba882424028
Lucas Emanuel <lerc.rj@hotmail.com>
2025-04-02 14:14:10 -0300

    first commit

---
+++ src/main/java/br/com/luscaer/api_scrum/ApiScrumApplication.java
@@ -0,0 +1,13 @@
+package br.com.luscaer.api_scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiScrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiScrumApplication.class, args);
+	}
+
+}
